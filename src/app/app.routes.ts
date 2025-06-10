fcb98c6a06e0e51a6a9dbb37e71e71ffe7d43fb4
Pedro Silva <pedro.mape7@gmail.com>
2025-06-10 10:27:40 -0300

    feat: creating some components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+    },
+    {
+        path: "home",
+        component: HomeComponent,
+    },
+    {
+        path: "**",
+        component: NotFoundComponent,
+    },
+];
